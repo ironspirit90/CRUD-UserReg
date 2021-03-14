8f8efc1c9b110f09feda0e329fa87f0d51e1652e
unknown <karolissemenas@gmail.com>
2021-03-15 01:42:17 +0200

    Form component + index.js semi done

---
+++ components/CountryDropDown.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>{{ list.name }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "CountryDropDown",
+  props: {
+    list: Object
+  }
+};
+</script>
---
+++ components/Form.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div>
+      <h1>Register User</h1>
+    </div>
+    <div>
+      <h6>Email</h6>
+      <input type="email" v-model="user.email" />
+      <h6>Name</h6>
+      <input type="text" v-model="user.passwordOne" />
+      <h6>SureName</h6>
+      <input type="text" v-model="user.passworTwo" />
+      <h6>Select Country</h6>
+      <CountryDropDown
+        :list="item"
+        v-for="(item, index) in countrylist"
+        :key="index"
+      />
+      <!-- <select name="Country" id="country">
+        <option value="">
+        
+        </option>
+      </select> -->
+    </div>
+    <div>
+      <button>Register</button>
+      <button>User List</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import CountryDropDown from "@/components/CountryDropDown";
+export default {
+  name: "Form",
+  components: {
+    CountryDropDown
+  },
+  props: {
+    countrylist: Array
+  },
+  data() {
+    return {
+      user: {
+        email: "",
+        passwordOne: "",
+        passwordTwo: "",
+        country: ""
+      },
+      err: false
+    };
+  },
+  methods: {
+    register() {
+      let mailRegex = /^\S+@\S+\.\S+$/;
+      let isEmailLegit = mailRegex.test(this.user.email);
+      if (isEmailLegit && this.user.email.length > 0) {
+      } else {
+        this.err = true;
+        console.log(this.err);
+      }
+    }
+  }
+  //   computed: {
+  //     countryApi() {
+  //       return this.$store.getters.getCountry;
+  //     }
+  //   },
+  //   fetch({ store }) {
+  //     return store.dispatch("getCountryApi");
+  //   }
+};
+</script>
